a19fc046de2d5c67f57f7b4e3898e12f3cb210c9
Louis <karpx033@umn.edu>
2022-02-27 14:43:02 -0600

    added a footer

--- my-portfolio/src/components/Bottom.js
+++ my-portfolio/src/components/Bottom.js
@@ -1,8 +1,16 @@
+import Card from 'react-bootstrap/Card';
+
 function Bottom() {
 return (
-    <div>
-    <h1>Bottom Section</h1>
-    </div>
+    <Card id="bot">
+        <Card.Footer>
+            <div style={{justifyContent: 'space-evenly', display: 'flex'}}>
+            <span> Louis Karp</span>
+            <span>612.500.2948</span>
+            <span>Karpx033@umn.edu</span>
+            </div>
+        </Card.Footer>
+    </Card>
 )
 }
 
